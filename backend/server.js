fc104057105cc252ad97878c242b6da5ea7c4548
du-213949 <daniel.unru@hs-weingarten.de>
2023-11-18 16:25:58 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const port = 8080;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Verbindung
+mongoose.connect("mongodb://localhost:27017/", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const professorSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+});
+
+const Professor = mongoose.model("Professor", professorSchema);
+
+// Endpoints
+
+// GET alle Professoren
+app.get("/profs", async (req, res) => {
+  try {
+    const professors = await Professor.find();
+    res.json(professors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET Professor nach ID
+app.get("/profs/:id", getProfessor, (req, res) => {
+  res.json(res.professor);
+});
+
+// POST neuer Professor
+app.post("/profs", async (req, res) => {
+  const professor = new Professor({
+    name: req.body.name,
+    rating: req.body.rating,
+  });
+
+  try {
+    const newProfessor = await professor.save();
+    res.status(201).json(newProfessor);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT (Aktualisierung) eines Professors
+app.put("/profs/:id", getProfessor, async (req, res) => {
+  if (req.body.name != null) {
+    res.professor.name = req.body.name;
+  }
+  if (req.body.rating != null) {
+    res.professor.rating = req.body.rating;
+  }
+
+  try {
+    const updatedProfessor = await res.professor.save();
+    res.json(updatedProfessor);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE eines Professors
+app.delete("/profs/:id", getProfessor, async (req, res) => {
+  try {
+    await res.professor.remove();
+    res.json({ message: "Professor gelöscht" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getProfessor(req, res, next) {
+  try {
+    const professor = await Professor.findById(req.params.id);
+    if (professor == null) {
+      return res.status(404).json({ message: "Professor nicht gefunden" });
+    }
+    res.professor = professor;
+    next();
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
